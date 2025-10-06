dcd8e8053d928053c394d15d15bd1566a91bc0d0
Rodolfo Franca <souzafrodolfo@gmail.com>
2025-10-06 17:46:34 -0300

    add systemds

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,10 @@ if status is-interactive
     set -U fish_user_paths $fish_user_paths ~/.config/sway/scripts/launchers/
     set -U fish_user_paths $fish_user_paths ~/.config/sway/scripts/media_sharing/
 
+    set -x XDG_CURRENT_DESKTOP sway   # xdg-desktop-portal
+    set -x XDG_SESSION_DESKTOP sway   # systemd
+    set -x XDG_SESSION_TYPE wayland
+
 end
 
 
